b7bff8dc101d4cb245dd1acfe56740e7995ef73b
Jeewana Kasun Siriwardana <jeewanakasun90@gmail.com>
2025-04-04 13:07:44 +0530

    Updated support scripts | docker

--- test/start.sh
+++ test/start.sh
@@ -7,13 +7,3 @@ ochestration=$(jq -r '.automationConfig.ochestration' config.json)
 backendType=$(jq -r '.automationConfig.backendType' config.json)
 
 sh sample.sh $backendType
-
-elif [ "$ochestration" = "k8s" ]; then
-
-    echo "|||||||||||||||||||||||||||||"
-    echo "Kubernetes execution is not yet implemented"
-    echo "|||||||||||||||||||||||||||||"
-    
-else
-    echo "It's not a valid option... Bye Bye !!!"
-fi
