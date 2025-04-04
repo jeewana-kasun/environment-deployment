5536031122daabef63b5e616da5606984c697829
Jeewana Kasun Siriwardana <jeewanakasun90@gmail.com>
2025-04-04 16:49:29 +0530

    Updated support scripts | docker

--- test/start.sh
+++ test/start.sh
@@ -3,7 +3,15 @@ folderPath="../projects/backend"
 
 # Check if .csproj or .sln files exist
 if ls $folderPath/*.csproj $folderPath/*.sln 1> /dev/null 2>&1; then
-    echo "This folder contains a .NET project."
+    echo "|||||||||||||||||||||||||||||"
+    echo "Backend Project folder contains a .NET project."
+    echo "|||||||||||||||||||||||||||||"
+elif ls $folderPath/requirements.txt $folderPath/*.py $folderPath/pyproject.toml 1> /dev/null 2>&1; then
+    echo "|||||||||||||||||||||||||||||"
+    echo "Backend Project folder contains a python project."
+    echo "|||||||||||||||||||||||||||||"
 else
-    echo "No .NET project found."
+    echo "|||||||||||||||||||||||||||||"
+    echo "No .NET or python project found."
+    echo "|||||||||||||||||||||||||||||"
 fi
